0aedfb9b6d1585ede615110544498ab916bcc929
happicamper <gdtoledano@gmail.com>
2026-08-10 13:28:48 +0800

    Added lint in the pipeline

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+const js = require("@eslint/js");
+const jest = require("eslint-plugin-jest");
+
+module.exports = [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "commonjs",
+            globals: {
+                require: "readonly",
+                module: "readonly"
+            }
+        }
+    },
+
+    {
+        files: ["tests/**/*.js"],
+        ...jest.configs["flat/recommended"]
+    },
+
+    js.configs.recommended
+];
